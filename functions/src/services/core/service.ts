b405d5d025497466ccd565dac77a90abb66509bf
James <james@thecubicle.us>
2020-10-23 15:03:57 -0400

    Upgrade jomql to latest, update core/service.ts

--- functions/src/services/core/service.ts
+++ functions/src/services/core/service.ts
@@ -1,6 +1,11 @@
-import errorHelper from '../../helpers/tier0/error';
+import errorHelper from "../../helpers/tier0/error";
 
-import { resolverHelper, jomqlHelper, subscriptionHelper, getTypeDefs } from 'jomql'
+import {
+  resolverHelper,
+  jomqlHelper,
+  subscriptionHelper,
+  getTypeDefs,
+} from "jomql";
 
 export abstract class Service {
   static __typename: string;
@@ -8,7 +13,7 @@ export abstract class Service {
   static paginator?: Service;
 
   static presets: {
-    default: {}
+    default: {};
   };
 
   static filterFieldsMap: Object = {};
@@ -24,15 +29,15 @@ export abstract class Service {
   static searchFieldsMap: Object = {};
 
   static isFilterRequired: Boolean = false;
-  
+
   static permissionsLink?: any;
 
   static accessControl?: {
-    get?: Function,
-    getMultiple?: Function,
-    create?: Function,
-    update?: Function,
-    delete?: Function,
+    get?: Function;
+    getMultiple?: Function;
+    create?: Function;
+    update?: Function;
+    delete?: Function;
   };
 
   static getTypeDef() {
@@ -40,151 +45,196 @@ export abstract class Service {
   }
 
   static async testPermissions(operation: string, req, args, query) {
-    if(!req.cache) {
+    if (!req.cache) {
       req.cache = {};
     }
 
     //check if this operation was already done. if so, return that result
-    if((operation + '-' + this.__typename) in req.cache) {
-      return req.cache[operation + '-' + this.__typename]
+    if (operation + "-" + this.__typename in req.cache) {
+      return req.cache[operation + "-" + this.__typename];
     }
-    
+
     let allowed: Boolean;
 
-    if(this.accessControl && (operation in this.accessControl)) {
+    if (this.accessControl && operation in this.accessControl) {
       allowed = await this.accessControl[operation](req, args, query);
     } else {
       allowed = true;
     }
 
     //cache the permissions check in req if not already there
-    return (req.cache[operation + '-' + this.__typename] = allowed);
+    return (req.cache[operation + "-" + this.__typename] = allowed);
   }
-  
-  static async subscribeToSingleItem(operationName: string, req, args, query?: object, admin = false) {
+
+  static async subscribeToSingleItem(
+    operationName: string,
+    req,
+    args,
+    query?: object,
+    admin = false
+  ) {
     const selectQuery = query || Object.assign({}, this.presets.default);
 
     //if it does not pass the access control, throw an error
-    if(!admin && !await this.testPermissions('get', req, args, query)) {
+    if (!admin && !(await this.testPermissions("get", req, args, query))) {
       throw errorHelper.badPermissionsError();
     }
 
     //check if the record and query is fetchable
-    const results = await resolverHelper.resolveTableRows(this.__typename, this, req, {
-      select: selectQuery,
-      where: [
-        {
-          fields: [
-            { field: "id", value: args.id }
-          ]
-        }
-      ]
+    const results = await resolverHelper.resolveTableRows(
+      this.__typename,
+      this,
+      req,
+      {
+        select: selectQuery,
+        where: [
+          {
+            fields: [{ field: "id", value: args.id }],
+          },
+        ],
+      },
+      args
+    );
 
-    }, args);
-
-    if(results.length < 1) {
+    if (results.length < 1) {
       throw errorHelper.itemNotFoundError();
     }
 
     const validatedArgs = {
-      id: args.id
+      id: args.id,
     };
 
-    const channel = await subscriptionHelper.handleJqlSubscription(req, operationName, validatedArgs, query || Object.assign({}, this.presets.default))
+    const channel = await subscriptionHelper.handleJqlSubscription(
+      req,
+      operationName,
+      validatedArgs,
+      query || Object.assign({}, this.presets.default)
+    );
 
     return {
-      channel_name: channel
+      channel_name: channel,
     };
   }
 
-  static async subscribeToMultipleItem(operationName: string, req, args, query?: object, admin = false) {
+  static async subscribeToMultipleItem(
+    operationName: string,
+    req,
+    args,
+    query?: object,
+    admin = false
+  ) {
     const selectQuery = query || Object.assign({}, this.presets.default);
 
     //if it does not pass the access control, throw an error
-    if(!admin && !await this.testPermissions('getMultiple', req, args, query)) {
+    if (
+      !admin &&
+      !(await this.testPermissions("getMultiple", req, args, query))
+    ) {
       throw errorHelper.badPermissionsError();
     }
-    
+
     //check if the query is valid (no need to actually run it)
     jomqlHelper.validateJsonqlQuery(selectQuery, this.__typename);
 
     const validatedArgs = {
-      created_by: args.created_by
+      created_by: args.created_by,
     };
 
-    const channel = await subscriptionHelper.handleJqlSubscription(req, operationName, validatedArgs, query || Object.assign({}, this.presets.default))
+    const channel = await subscriptionHelper.handleJqlSubscription(
+      req,
+      operationName,
+      validatedArgs,
+      query || Object.assign({}, this.presets.default)
+    );
 
-    return { 
-      channel_name: channel
+    return {
+      channel_name: channel,
     };
   }
 
-  static async getRecord(req, args = <any> {}, query?: object, admin = false) {
+  static async getRecord(req, args = <any>{}, query?: object, admin = false) {
     const selectQuery = query || Object.assign({}, this.presets.default);
 
     //if it does not pass the access control, throw an error
-    if(!admin && !await this.testPermissions('get', req, args, query)) {
+    if (!admin && !(await this.testPermissions("get", req, args, query))) {
       throw errorHelper.badPermissionsError();
     }
 
     const filterArray: Array<any> = [];
-    if(this.hasKeys) {
+    if (this.hasKeys) {
       //handle filter fields
-      for(const arg in args) {
-        if(arg in this.filterFieldsKeyMap) {
+      for (const arg in args) {
+        if (arg in this.filterFieldsKeyMap) {
           const filterObject = {
             connective: "AND",
-            fields: <any> []
+            fields: <any>[],
           };
           filterObject.fields.push({
             field: this.filterFieldsKeyMap[arg].field ?? arg,
             joinFields: this.filterFieldsKeyMap[arg].joinFields,
-            value: args[arg]
+            value: args[arg],
           });
           filterArray.push(filterObject);
         }
       }
 
-      if(filterArray.length < 1) {
-        throw errorHelper.generateError("Must supply at least 1 filter parameter");
+      if (filterArray.length < 1) {
+        throw errorHelper.generateError(
+          "Must supply at least 1 filter parameter"
+        );
       }
     }
 
-    const results = await resolverHelper.resolveTableRows(this.__typename, this, req, {
-      select: selectQuery,
-      where: filterArray,
-      limit: 1,
-    }, args);
+    const results = await resolverHelper.resolveTableRows(
+      this.__typename,
+      this,
+      req,
+      {
+        select: selectQuery,
+        where: filterArray,
+        limit: 1,
+      },
+      args
+    );
 
-    if(results.length < 1) {
+    if (results.length < 1) {
       throw errorHelper.itemNotFoundError();
     }
 
     return results[0];
   }
 
-  static async getRecords(req, args = <any> {}, query?: object, count = false, admin = false) {
+  static async getRecords(
+    req,
+    args = <any>{},
+    query?: object,
+    count = false,
+    admin = false
+  ) {
     const selectQuery = query || Object.assign({}, this.presets.default);
 
     //if it does not pass the access control, throw an error
-    if(!admin && !await this.testPermissions('getMultiple', req, args, query)) {
+    if (
+      !admin &&
+      !(await this.testPermissions("getMultiple", req, args, query))
+    ) {
       throw errorHelper.badPermissionsError();
     }
 
     const filterArray: Array<any> = [];
-    
+
     //handle filter fields
-    if(args.filterBy && typeof args.filterBy === "object") {
-      for(const arg in args.filterBy) {
-        if(arg in this.filterFieldsMap) {
+    if (args.filterBy && typeof args.filterBy === "object") {
+      for (const arg in args.filterBy) {
+        if (arg in this.filterFieldsMap) {
           const filterObject = {
             connective: "AND",
-            fields: <any> []
+            fields: <any>[],
           };
           filterObject.fields.push({
             field: this.filterFieldsMap[arg].field ?? arg,
             joinFields: this.filterFieldsMap[arg].joinFields,
-            value: args.filterBy[arg]
+            value: args.filterBy[arg],
           });
           filterArray.push(filterObject);
         }
@@ -192,135 +242,178 @@ export abstract class Service {
     }
 
     //handle search fields
-    if(args.search) {
+    if (args.search) {
       const filterObject = {
         connective: "OR",
-        fields: <any> []
+        fields: <any>[],
       };
 
-      for(const prop in this.searchFieldsMap) {
+      for (const prop in this.searchFieldsMap) {
         filterObject.fields.push({
           field: this.searchFieldsMap[prop].field ?? prop,
           joinFields: this.searchFieldsMap[prop].joinFields,
-          value: '%' + args.search + '%', 
-          operator: 'LIKE'
+          value: "%" + args.search + "%",
+          operator: "LIKE",
         });
       }
-      
+
       filterArray.push(filterObject);
     }
 
-    if(filterArray.length < 1 && this.isFilterRequired) {
-      throw errorHelper.generateError("Must supply at least 1 filter parameter");
+    if (filterArray.length < 1 && this.isFilterRequired) {
+      throw errorHelper.generateError(
+        "Must supply at least 1 filter parameter"
+      );
     }
 
-    if(count) {
-      //add the "after" constraint, if provided
-      if(args.after) {
-        filterArray.push({
-          fields: {
-            id: { value: args.after, operator: '>' }
-          }
-        });
-      }
-      
-      const resultsCount = await resolverHelper.countTableRows(this.__typename, filterArray);
-  
+    //add the "after" constraint, if provided
+    if (args.after) {
+      const operator =
+        Array.isArray(args.sortDesc) && args.sortDesc[0] === true ? "<" : ">";
+
+      filterArray.push({
+        fields: [{ field: "id", value: args.after, operator }],
+      });
+    }
+
+    if (count) {
+      const resultsCount = await resolverHelper.countTableRows(
+        this.__typename,
+        filterArray
+      );
+
       return resultsCount;
     } else {
       //parse args.first and ensure it is less than 100
-      const limit = parseInt(args.first) > 100 ? 100 : (parseInt(args.first) || 100);
+      const limit =
+        parseInt(args.first) > 100 ? 100 : parseInt(args.first) || 100;
 
-      const results = await resolverHelper.resolveTableRows(this.__typename, this, req, {
-        select: selectQuery,
-        where: filterArray,
-        orderBy: Array.isArray(args.sortBy) ? args.sortBy.reduce((total, item, index) => {
-          if(item in this.sortFieldsMap) {
-            total.push({
-              field: this.sortFieldsMap[item].field ?? item,
-              desc: Array.isArray(args.sortDesc) ? (args.sortDesc[index] === "true" || args.sortDesc[index] === true) : true
-            });
-          }
-          return total;
-        }, []) : null,
-        limit: limit,
-        after: args.after,
-        groupBy: Array.isArray(args.groupBy) ? args.groupBy.reduce((total, item, index) => {
-          if(item in this.groupByFieldsMap) {
-            total.push({
-              field: this.groupByFieldsMap[item].field ?? item
-            });
-          }
-          return total;
-        }, []) : null,
-        //offset: args.first*args.page || 0
-      }, args);
-  
+      const results = await resolverHelper.resolveTableRows(
+        this.__typename,
+        this,
+        req,
+        {
+          select: selectQuery,
+          where: filterArray,
+          orderBy: Array.isArray(args.sortBy)
+            ? args.sortBy.reduce((total, item, index) => {
+                if (item in this.sortFieldsMap) {
+                  total.push({
+                    field: this.sortFieldsMap[item].field ?? item,
+                    desc: Array.isArray(args.sortDesc)
+                      ? args.sortDesc[index] === "true" ||
+                        args.sortDesc[index] === true
+                      : true,
+                  });
+                }
+                return total;
+              }, [])
+            : null,
+          limit: limit,
+          groupBy: Array.isArray(args.groupBy)
+            ? args.groupBy.reduce((total, item, index) => {
+                if (item in this.groupByFieldsMap) {
+                  total.push({
+                    field: this.groupByFieldsMap[item].field ?? item,
+                  });
+                }
+                return total;
+              }, [])
+            : null,
+          //offset: args.first*args.page || 0
+        },
+        args
+      );
+
       return results;
     }
   }
 
-  static async createRecord(req, args = <any> {}, query?: object, admin = false) {
+  static async createRecord(
+    req,
+    args = <any>{},
+    query?: object,
+    admin = false
+  ) {
     //if it does not pass the access control, throw an error
-    if(!admin && !await this.testPermissions('create', req, args, query)) {
+    if (!admin && !(await this.testPermissions("create", req, args, query))) {
       throw errorHelper.badPermissionsError();
     }
 
-    const addResults = await resolverHelper.addTableRow(this.__typename, {
-      ...args,
-    }, {
-      created_by: req.user.id
-    });
+    const addResults = await resolverHelper.addTableRow(
+      this.__typename,
+      {
+        ...args,
+      },
+      {
+        created_by: req.user.id,
+      }
+    );
 
     const validatedArgs = {
-      created_by: req.user.id
+      created_by: req.user.id,
     };
 
-    subscriptionHelper.handleJqlSubscriptionTriggerIterative(req, this, this.__typename + 'Created', validatedArgs, { id: addResults.id });
+    subscriptionHelper.handleJqlSubscriptionTriggerIterative(
+      req,
+      this,
+      this.__typename + "Created",
+      validatedArgs,
+      { id: addResults.id }
+    );
 
     return this.getRecord(req, { id: addResults.id }, query);
   }
 
-  static async updateRecord(req, args = <any> {}, query?: object) {
+  static async updateRecord(req, args = <any>{}, query?: object) {
     //if it does not pass the access control, throw an error
-    if(!await this.testPermissions('update', req, args, query)) {
+    if (!(await this.testPermissions("update", req, args, query))) {
       throw errorHelper.badPermissionsError();
     }
 
     //check if record exists
-    const recordExistCount = await resolverHelper.countTableRows(this.__typename, [
-      {
-        fields: [
-          { field: "id", value: args.id }
-        ]
-      }
-    ]);
+    const recordExistCount = await resolverHelper.countTableRows(
+      this.__typename,
+      [
+        {
+          fields: [{ field: "id", value: args.id }],
+        },
+      ]
+    );
 
-    if(recordExistCount < 1) {
-      throw errorHelper.generateError('Item not found', 404);
+    if (recordExistCount < 1) {
+      throw errorHelper.generateError("Item not found", 404);
     }
-    
-    await resolverHelper.updateTableRow(this.__typename, {
-      ...args,
-      updated_at: null
-    }, [{ fields: [ { field: "id", value: args.id } ] }]);
-    
+
+    await resolverHelper.updateTableRow(
+      this.__typename,
+      {
+        ...args,
+        updated_at: null,
+      },
+      [{ fields: [{ field: "id", value: args.id }] }]
+    );
 
     const returnData = this.getRecord(req, { id: args.id }, query);
 
     //check subscriptions table where companyUpdated and id = 1.
     const validatedArgs = {
-      id: args.id
+      id: args.id,
     };
 
-    subscriptionHelper.handleJqlSubscriptionTrigger(req, this, this.__typename + 'Updated', validatedArgs);
+    subscriptionHelper.handleJqlSubscriptionTrigger(
+      req,
+      this,
+      this.__typename + "Updated",
+      validatedArgs
+    );
 
     return returnData;
   }
 
-  static async deleteRecord(req, args = <any> {}, query?: object) {
+  static async deleteRecord(req, args = <any>{}, query?: object) {
     //if it does not pass the access control, throw an error
-    if(!await this.testPermissions('delete', req, args, query)) {
+    if (!(await this.testPermissions("delete", req, args, query))) {
       throw errorHelper.badPermissionsError();
     }
 
@@ -329,35 +422,44 @@ export abstract class Service {
 
     //check subscriptions table where companyDeleted and id = 1.
     const validatedArgs = {
-      id: args.id
+      id: args.id,
     };
 
-    await subscriptionHelper.handleJqlSubscriptionTrigger(req, this, this.__typename + 'Deleted', validatedArgs);
+    await subscriptionHelper.handleJqlSubscriptionTrigger(
+      req,
+      this,
+      this.__typename + "Deleted",
+      validatedArgs
+    );
 
     await resolverHelper.deleteTableRow(this.__typename, args, [
       {
-        fields: [
-          { field: "id", value: args.id }
-        ]
-      }
+        fields: [{ field: "id", value: args.id }],
+      },
     ]);
 
     //cleanup
 
     //also need to delete all subscriptions for this item
-    await subscriptionHelper.deleteJqlSubscription(req, this.__typename + 'Deleted', validatedArgs);
+    await subscriptionHelper.deleteJqlSubscription(
+      req,
+      this.__typename + "Deleted",
+      validatedArgs
+    );
 
     //also need to delete all permissions attached to this item
-    if(this.permissionsLink) {
-      await resolverHelper.deleteTableRow(this.permissionsLink.__typename, args, [
-        {
-          fields: [
-            { field: this.__typename, value: args.id }
-          ]
-        }
-      ]);
+    if (this.permissionsLink) {
+      await resolverHelper.deleteTableRow(
+        this.permissionsLink.__typename,
+        args,
+        [
+          {
+            fields: [{ field: this.__typename, value: args.id }],
+          },
+        ]
+      );
     }
 
     return requestedResults;
   }
-};
+}
